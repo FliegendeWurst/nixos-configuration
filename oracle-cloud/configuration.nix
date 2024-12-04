74c46376a64f6d0d46c5291e38a355270745ffe4
FliegendeWurst <2012gdwu+github@posteo.de>
2024-12-04 21:55:51 +0100

    oracle-cloud: update NUR and set vim as default

--- oracle-cloud/configuration.nix
+++ oracle-cloud/configuration.nix
@@ -29,9 +29,9 @@
   ) ];
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/archive/3bf72a07ced4187061498ef42dc6cfa901e80194.tar.gz";
+      url = "https://github.com/nix-community/NUR/archive/9a26641139d1b8f59059fad2500fbf06ca5918ab.tar.gz";
       # Get the hash by running `nix-prefetch-url --unpack <url>` on the above url
-      sha256 = "1bdxd3f0fqw89l73y9k881qgn8waiqxwy0zm4ga9si8xadw56y34";
+      sha256 = "1q48l5fvr0fyavvikmw9r6bn8dypxxvxh2phb8asyzwjl46f4i0z";
     }) {
       inherit pkgs;
     };
@@ -220,6 +220,8 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "2012gdwu@posteo.de";
 
+  programs.vim.enable = true;
+  programs.vim.defaultEditor = true;
   environment.systemPackages = with pkgs; [
     vim
     htop
