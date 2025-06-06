5b9a15b269534097126c002692dadee93c5d1e16
FliegendeWurst <2012gdwu+github@posteo.de>
2025-06-06 16:50:47 +0200

    laptop: configure platformio udev

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -390,4 +390,6 @@ rec {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+
+  services.udev.packages = with pkgs; [ platformio-core.udev ];
 }
