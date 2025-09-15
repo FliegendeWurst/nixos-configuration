83f5eed924680308e85ef8ea41ff288be2dfde4c
FliegendeWurst <arne.keller@posteo.de>
2025-09-15 11:13:32 +0200

    home: update Lix

--- home/flake.nix
+++ home/flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-tree = {
